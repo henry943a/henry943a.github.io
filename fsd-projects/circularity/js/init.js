219c7baa48d459061be02294319d499af9f44d05
henry943a <henry943@wearescpps.org>
2025-11-20 17:16:00 +0000

    message

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,11 +20,12 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
-
+var circle; 
+var circles = [];
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+
+        }
 
 
         // TODO 3 : Call the drawCircle() function
